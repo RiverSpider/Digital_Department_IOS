7927ff12196f648a0fd0e2fdcd3b75a12bfa0dcc
Nailloon <artemfilyakin5@gmail.com>
2023-11-17 23:25:47 +0100

    Initial Commit

---
+++ Digital_Department_IOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Digital_Department_IOS
+//
+//  Created by user225687 on 11/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Digital_Department_IOS/Digital_Department_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_Department_IOSApp.swift
+//  Digital_Department_IOS
+//
+//  Created by user225687 on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Digital_Department_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
